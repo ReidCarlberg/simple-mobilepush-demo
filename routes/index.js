5d8cc0876dfaa9cc76ca74d8b81aab93de0bd7c0
Reid Carlberg <reid.carlberg@gmail.com>
2015-06-14 07:51:05 -0500

    full express scaffolding

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CNX15' });
+  res.render('index', { title: 'MobilePush@CNX15' });
 });
 
 module.exports = router;
