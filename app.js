0596ff0b616a41925c24c3ddb161fea52c27084c
Reid Carlberg <reid.carlberg@gmail.com>
2015-06-20 05:28:15 -0500

    minor cleanup

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/messages', messages);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MobilePush API / Node / Express' });
+  res.render('index', { title: 'Simple MobilePush Demo' });
 });
 
 module.exports = router;
