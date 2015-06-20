6db5abc61d0e90e43fd790a64780a29322515ea2
Reid Carlberg <reid.carlberg@gmail.com>
2015-06-20 05:37:56 -0500

    minor cleanup

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var messages = require('./routes/messages');
 
 var app = express();
